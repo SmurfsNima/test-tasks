771c200dfaf4686cbe3556c8519a668164a0edc6
SmurfsNIma <sadeghinima090@gmail.com>
2024-05-20 20:05:54 +0330

    some bugs fixed

--- src/components/PostSection.tsx
+++ src/components/PostSection.tsx
@@ -1,14 +1,10 @@
 import { Comment } from "@mui/icons-material";
-import SendIcon from "@mui/icons-material/Send";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useState } from "react";
-
 interface Comment {
   text: string;
   email: string;
-
- 
 }
 interface PostsectionProps {
   post: {
@@ -20,14 +16,13 @@ interface PostsectionProps {
 export const PostSection = ({ post }: PostsectionProps) => {
   const { title, description } = post;
   const [comments, setComments] = useState(post.comments);
-  const [isLiked , setIsliked] = useState(false)
+  const [isLiked, setIsliked] = useState(false);
   const handleDeleteCommnet = (index: number, email: string) => {
     if (comments[index].email === email) {
       const newComments = comments.filter((_, i) => i !== index);
       setComments(newComments);
     }
   };
-
   return (
     <div className="px-6 py-3 flex flex-col shadow-lg  bg-gray-100 rounded-lg">
       <div className="py-2 px-4 flex flex-col border-b border-black ">
@@ -41,11 +36,12 @@ export const PostSection = ({ post }: PostsectionProps) => {
             <div className="flex justify-between">
               <p className="text-[18px] font-semibold">{comment.text}</p>
               <div className="flex gap-1 items-center">
-                <div onClick={()=>setIsliked(!isLiked) }><FavoriteIcon  style={{color: isLiked ? 'red' : 'gray'}}/></div>
-                
+                <div onClick={() => setIsliked(!isLiked)}>
+                  <FavoriteIcon style={{ color: isLiked ? "red" : "gray" }} />
+                </div>
+
                 <div onClick={() => handleDeleteCommnet(index, comment.email)}>
-                  <DeleteIcon  />
-                
+                  <DeleteIcon />
                 </div>
               </div>
             </div>
@@ -54,5 +50,4 @@ export const PostSection = ({ post }: PostsectionProps) => {
       </ul>
     </div>
   );
-  
 };
